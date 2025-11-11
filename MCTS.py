2bf29e5628b614ee3631554da90d53e764597b23
ivespino <ivespino08@gmail.com>
2025-11-11 14:31:52 -0700

    Column full check added

--- MCTS.py
+++ MCTS.py
@@ -7,20 +7,29 @@ class Connect4Tree:
         self.board = np.full((6,7),"O")
 
     def drop(self, column, color):
-
         i = 0
         while i < len(self.board) and self.board[i][column] == "O":
             i += 1
 
+        if i == 0:
+            self.column_full(column+1)
+            return False
         self.board[i-1][column] = color
+
+        return True
+
+    def column_full(column):
+        print(f"Error: Column {column} is full please choose another")
+
 
 
 
 
 
 def uniform_random(board):
+    
     col_chosen = randint(0, len(board.board[0])-1)
-    board.drop(col_chosen, "Y")
+    success = board.drop(col_chosen, "Y")
     
 
 if __name__ == "__main__":
